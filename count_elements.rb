9f9f5c777bf8051a40e074e3a3f05f9f5689e683
Shoaib19 <shoaibsabir019@gmail.com>
2023-04-22 02:04:13 +0500

    both problems test cases are passed

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,13 @@
 class CountElements
+  def initialize(nums)
+    @nums = nums
+  end
 
+  def perform
+    nums.each_with_object(Hash.new(0)) { |num, hash| hash[num] += 1 }
+  end
+
+  private
+
+  attr_reader :nums
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,14 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    regex = /^(?!.*(.)(?:.*\1){2})(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>]).{6,24}$/
+    password =~ regex ? true : false
+  end
+
+  private
+
+  attr_reader :password
 end
